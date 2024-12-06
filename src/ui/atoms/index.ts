b895f1600070c33cec8399c22addc8820bdde068
sebas1913 <sebasosornicastrillon@gmail.com>
2024-12-06 18:07:40 -0500

    Fix

--- src/ui/atoms/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Icons';
-export * from './Label';
-export * from './Paragraph';
-export * from './Title';
-export * from './button/Button';
-export * from './input/Input';
-export * from './spinner/Spinner';
